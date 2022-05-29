d09a3b30e8b9b0742d6876ac87fe22a4f79e1ff3
Ahmed Farag <91483335+AhmedFarag57@users.noreply.github.com>
2022-05-29 21:03:29 +0200

    Delete basmala directory

--- basmala/SE_FINAL_PROJECT-master/resources/views/backend/patients/create.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-
-    <h1>create patients</h1>
-@endsection
--- basmala/SE_FINAL_PROJECT-master/resources/views/backend/patients/show.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    
-    <h1>Name is : {{ $patient->name }}</h1>
-
-@endsection
